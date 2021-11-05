8ac64a918346181e986b068aeae89b666eb9fc53
Frank Urbach <frank.urbach@gmx.de>
2021-11-05 16:43:03 +0100

    Work towards first implementation of Relations !Caution! not working yet.

--- src/TypeDBORM.jl
+++ src/TypeDBORM.jl
@@ -1,12 +1,17 @@
 module TypeDBORM
 
 using TypeDBClient: ConceptMap, AbstractAttributeType, Proto, ConceptManager, AbstractThingType
-using TypeDBClient: AbstractCoreTransaction, AbstractAttribute
-using TypeDBClient: as_remote, create, get_has, get_instances, put, set_has
+using TypeDBClient: AbstractCoreTransaction, AbstractAttribute, AbstractThing, AttributeType
+using TypeDBClient: AbstractRelationType, Label
+using TypeDBClient: as_remote, create, get_has, get_instances, put, set_has, unset_has
+using TypeDBClient: add_player, get_relations
 using TypeDBClient
+using Dates
 
 include("read_write_orm.jl")
 
-export read_concept_from_struct, write_struct
+export read_concept_from_struct, write_struct, default_value_of
+
+const Direct_Storables = Union{AbstractString, Number, Bool, Date, DateTime}
 
 end
