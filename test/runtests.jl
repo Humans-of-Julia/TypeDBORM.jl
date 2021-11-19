45817e4f7d417d307ef868f7e16d98dcf1212ea6
Frank Urbach <frank.urbach@gmx.de>
2021-11-19 01:02:05 +0100

    tests corrected

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,10 @@ using Test
 
 using TypeDBORM: _resulting_fieldtype
 
- include(joinpath(dirname(@__FILE__),"types_structs.jl"))
+include(joinpath(dirname(@__FILE__),"types_structs.jl"))
 
 @testset "util_functions" begin
-    rel_type = _resulting_fieldtype(TestTypeStruct, :address_rel)
+    rel_type = _resulting_fieldtype(Employee, :address_rel)
     @info rel_type
     @test rel_type == Address
 end
